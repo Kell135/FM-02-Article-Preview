15bade6d05835ad41e71c3a59c6fb1a3f0338aac
Kelly <kelly@ks.com>
2020-10-02 16:44:41 +0200

    size fix for mobile, share box in the bottom for mobile version

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -15,30 +15,32 @@ const Content = () => {
   };
 
   return (
-    <div className="content">
-      <h1 className="title">
-        Shift the overall look and feel by adding these wonderful touches to
-        furniture in your home
-      </h1>
-      <p className="message">
-        Ever been in a room and felt like something was missing? Perhaps it felt
-        slightly bare and uninviting. I’ve got some simple tips to help you make
-        any room feel complete.
-      </p>
-      <Profile />
+    <div className="second-part">
+      <div className="content">
+        <h1 className="title">
+          Shift the overall look and feel by adding these wonderful touches to
+          furniture in your home
+        </h1>
+        <p className="message">
+          Ever been in a room and felt like something was missing? Perhaps it
+          felt slightly bare and uninviting. I’ve got some simple tips to help
+          you make any room feel complete.
+        </p>
+        <Profile />
+      </div>
       <div
         className="icon-circle"
         style={{
           backgroundColor: isHidden
             ? "hsl(210, 46%, 95%)"
-            : "hsl(217, 19%, 35%)",
+            : "hsl(214, 17%, 51%)",
         }}
       >
         <i
           className="fas fa-share"
           onClick={handleClick}
           style={{
-            color: isHidden ? "non" : "white",
+            color: isHidden ? "hsl(217, 19%, 35%)" : "white",
           }}
         />
       </div>
